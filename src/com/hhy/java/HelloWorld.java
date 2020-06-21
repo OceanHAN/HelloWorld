4b7fa1d3c1d343109d892e80bfe4d79f6ce5a23d
ndouser <java0215@163.com>
2020-06-21 21:43:45 +0800

    11

--- src/com/hhy/java/HelloWorld.java
+++ src/com/hhy/java/HelloWorld.java
@@ -3,9 +3,7 @@ package com.hhy.java;
 public class HelloWorld {
     //psvm
     public static void main(String[] args) {
-        System.out.println("hello world"); //sout
-        int a = test();
-        System.out.println(a);
+        System.out.println("2020");
     }
 
     public static int test(){
